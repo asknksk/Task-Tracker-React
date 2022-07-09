87727eed85424099d9ac0e2011f36ec24768f829
Askin <asknkskk@gmail.com>
2022-07-09 15:04:37 +0300

    input added

--- src/components/AddTaskForm.jsx
+++ src/components/AddTaskForm.jsx
@@ -1,8 +1,40 @@
+import { useState } from "react";
 
 const AddTaskForm = () => {
+  const [task, setTask] = useState("");
+  const [day, setDay] = useState("");
   return (
-    <div>AddTaskForm</div>
-  )
-}
+    <div>
+      <form>
+        <div className="form-control">
+          <label htmlFor="task">Task</label>
+          <input
+            type="text"
+            name="text"
+            id="task"
+            placeholder="add task"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="day">Task</label>
+          <input
+            type="date"
+            name="day"
+            id="day"
+            value={day}
+            onChange={(e) => setDay(e.target.value)}
+          />
+        </div>
+        <div>
+          <button className="btn btn-submit" type="submit">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default AddTaskForm
+export default AddTaskForm;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,48 @@
 import AddTaskForm from "./AddTaskForm";
+import { useState } from "react";
+
+//! React, default olarak state'leri hemen degistirmeyebilir.
+//! Ekstra render'lari azaltmak icin state'leri toplu (batch) bir sekilde gunceller.
+//! Bir event handler icerisindeki ardasik state'ler event bitiminde toplu bir
+//! sekilde guncellenmis olur.State'lerin guncelenmesi gelis sirasina gorere yapilir.
+//! Ayni event icerisinde birbirine bagli state'leri kullanirken buna dikkat etmek gerkir.
+
+//? https://stackoverflow.com/questions/48563650/does-react-keep-the-order-for-state-updates
 
 const Header = () => {
+  const [show, setShow] = useState(false);
+  const [btnStyle, setBtnStyle] = useState({
+    name: "SHOW ADDTASK BAR",
+    bgColor: `purple`,
+  });
+
+  const handleShow = () => {
+    if (show) {
+      setBtnStyle({
+        name: "SHOW ADDTASK BAR",
+        bgColor: `purple`,
+      });
+    } else {
+      setBtnStyle({
+        name: "CLOSE ADDTASK BAR",
+        bgColor: `red`,
+      });
+    }
+    setShow(!show);
+  };
+  console.log(show);
+
   return (
-    <header>
+    <header className="header">
       <h1>TASK TRACKER</h1>
-      <AddTaskForm />
+      <button
+        className="btn"
+        onClick={handleShow}
+        style={{ backgroundColor: btnStyle.bgColor }}
+      >
+        {btnStyle.name}
+      </button>
+      {show && <AddTaskForm />}
     </header>
   );
 };
